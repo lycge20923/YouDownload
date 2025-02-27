cf72e167a92dc6762a36321b2a026061de916809
Lawrence Chang <>
2025-02-27 13:57:42 +0800

    adjust: add m4a to mp3 convert function

--- python_code/utils/download.py
+++ python_code/utils/download.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
+import subprocess
 
 def download_mp4(url:str, download_path:str) -> None:
 
@@ -18,11 +19,29 @@ def download_mp4(url:str, download_path:str) -> None:
     os.system(f'rm -r "{download_path}"')
     os.system(f'mv "{download_path}"_temp "{download_path}"')
 
+def convert_m4a_to_mp3(m4a_file_path, mp3_file_path):
+    
+    # Adjusted command to ensure compatibility
+    command = f'ffmpeg -i "{m4a_file_path}" -vn -ar 44100 -ac 2 -ab 192k -f mp3 "{mp3_file_path}"'
+    try:
+        subprocess.check_call(command, shell=True)
+        print(f"Conversion complete: {mp3_file_path}")
+    except subprocess.CalledProcessError:
+        print("Failed to convert file. Ensure FFmpeg is installed and the input file path is correct.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
 def download_mp3(url:str, download_path:str) -> None:
     yt = YouTube(url, on_progress_callback=on_progress, use_po_token=True)
     ys = yt.streams.get_audio_only()
     ys.download()
-    os.system(f'mv "{yt.title}.m4a" "{download_path}"')
+    if download_path.split('.')[-1] == "mp3":
+        convert_m4a_to_mp3(f"{yt.title}.m4a", download_path)
+        os.system(f'rm "{yt.title}.m4a"')
+    elif download_path.split('.')[-1] == "m4a":
+        os.system(f'mv "{yt.title}.m4a" "{download_path}"')
+    else:
+        raise ValueError("Unknown download file extention.")
 
 def addParsers():
     parser = argparse.ArgumentParser()
